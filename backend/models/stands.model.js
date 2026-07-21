b3b646662dbba893fa20df3efba7ee9f9f844db1
Maik <mcarvajal9911@gmail.com>
2026-07-21 15:31:19 -0600

    [fix]
    Funciona endpoint "POST" y "GET"

--- backend/models/stands.model.js
+++ backend/models/stands.model.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const schemaStand = new moongose.Schema({
+const schemaStand = new mongoose.Schema({
     nombre: {
         type: String,
         required: true
