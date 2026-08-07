edaac3a29564933971027f2836448543df8116db
Maik <mcarvajal9911@gmail.com>
2026-08-06 20:09:20 -0600

    [style]
    Diseña el modo oscuro de la pantalla donde se muestran los stands disponibles

--- frontend/js/listar-stands.js
+++ frontend/js/listar-stands.js
@@ -15,10 +15,7 @@ async function obtenerStands() {
         const primero = stands[0];
         let html = `
         <div class="col-12 col-lg-4">
-            <div class="card h-100 p-4 shadow-sm border border-dark border-2">
-                <div class="bg-light border border-dark border-2 d-flex align-items-center justify-content-center mb-4" style="width: 140px; height: 100px;">
-                    <span class="fw-bold tracking-wider">LOGO</span>
-                </div>
+            <div class="card fondo-tarjeta h-100 p-4 border border-dark border-2">
 
                 <div class="mb-4">
                     <p class="fs-5 mb-2"><strong>Nombre:</strong> ${primero.nombre}</p>
@@ -32,9 +29,7 @@ async function obtenerStands() {
                     </div>
                 </div>
 
-                <div class="mt-auto text-end">
-                    <a href="#" class="btn btn-outline-dark px-4 py-2 fw-medium">Ver Más</a>
-                </div>
+                
             </div>
         </div>`;
 
@@ -46,16 +41,18 @@ async function obtenerStands() {
                 const stand = stands[i];
                 html += `
                 <div class="col-12 col-md-6">
-                    <div class="card p-4 shadow-sm h-100 border border-dark d-flex flex-column justify-content-between">
+                    <div class="card fondo-tarjeta p-4  h-100 border border-dark d-flex flex-column justify-content-between">
                         <div>
                             <p class="mb-2"><strong>Nombre:</strong> ${stand.nombre}</p>
                             <p class="mb-2"><strong>Categoría:</strong> ${stand.categoria}</p>
                             <p class="mb-2"><strong>Responsable:</strong> ${stand.responsable}</p>
                             <p class="mb-0"><strong>Ubicación:</strong> ${stand.ubicacion}</p>
+                            <label class="form-label fw-bold fs-5 mb-1">Descripción:</label>
+                    <div class="border border-dark p-3 bg-white mb-4" style="min-height: 120px; border-radius: 4px;">
+                        <p class="text-muted small mb-0">${primero.descripcion}</p>
+                    </div>
                         </div>
-                        <div class="text-end mt-4">
-                            <a href="#" class="btn btn-outline-dark px-3 py-1 btn-sm fw-medium">Ver Más</a>
-                        </div>
+                        
                     </div>
                 </div>`;
             }
